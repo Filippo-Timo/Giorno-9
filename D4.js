12896bfbf7751be6b1608685a2560b243e0d66ee
Filippo-Timo <filtimo2002@gmail.com>
2025-10-02 16:36:22 +0200

    Updates js and html

--- D4.js
+++ D4.js
@@ -1,8 +1,14 @@
 /* ESERCIZIO 1
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const area = function (l1, l2) {
+  let risultato = l1 * l2;
+  return risultato;
+};
+
+console.log("L'area del rettangolo è ", area(5, 10));
 
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
@@ -10,21 +16,51 @@
  la loro somma moltiplicata per tre.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// NUMERI DIVERSI
+const crazySum1 = function (n1, n2) {
+  let somma = n1 + n2;
+  if (n1 != n2) {
+    return somma;
+  } else {
+    return somma * 3;
+  }
+};
+
+console.log("crazySum tra numeri DIVERSI = ", crazySum1(3, 5));
+console.log("crazySum tra numeri UGUALI = ", crazySum1(5, 5));
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// NUMERO MINORE DI 19
+const crazyDiff1 = function (n1, n2 = 19) {
+  let differenza = n1 - n2;
+  if (n1 <= 19) {
+    return differenza;
+  } else {
+    return differenza * 3;
+  }
+};
+
+console.log("crazyDiff di un numero MINORE di 19 = ", crazyDiff1(3));
+console.log("crazyDiff di un numero MAGGIORE di 19 = ", crazyDiff1(39));
 
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const boundary = function (n) {
+  if (20 <= n && n <= 100) {
+    return "true";
+  } else {
+    return "false";
+  }
+};
+
+console.log(boundary(36));
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
@@ -32,20 +68,49 @@
  ritornare la stringa originale senza alterarla.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const epify = function (str) {
+  if ((str = "EPICODE")) {
+    return str;
+  } else {
+    return "EPICODE" + text;
+  }
+};
+
+console.log("!= EPICODE...", epify("ehi come stai"));
+console.log("=== EPICODE...", epify("EPICODE"));
+
+// console.log(epify("epicode è fantastico"));
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const check3and7 = function (n) {
+  if (n % 3 === 0) {
+    console.log("Multiplo di 3");
+  } else if (n % 7 === 0) {
+    console.log("Multiplo di 7");
+  } else {
+    console.log("Non è multiplo di nessuno");
+  }
+};
+
+check3and7(35);
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const reverseString = function (str) {
+  let stringa = [];
+  stringa.push(str);
+  stringa.split(", ");
+  stringa.reverse();
+  return stringa;
+};
+
+console.log(reverseString("EPICODE"));
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
